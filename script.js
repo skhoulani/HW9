10e484939982c0ce5fb89010ae88b92e273cdb20
skhoulan <samir_khoulani@student.uml.edu>
2016-12-06 22:41:54 -0500

    Score updating correctly with multipliers

--- script.js
+++ script.js
@@ -20,6 +20,8 @@ $(document).ready(function() {
     var hand; //The current player hand with 7 tiles max
     var board; // board length of 15;
     var score = 0;
+    var currentSum = 0;
+    var tripleCount = 0;
     //creating the default new game environment
     dealHand(7);
     $("#scoreArea").html("Score: "+ score);
@@ -40,10 +42,21 @@ $(document).ready(function() {
     }
 
     $(".boardPiece").droppable({
+
         drop: function( event, ui ) {
             //console.log($(this));
             //console.log(parseInt(($(ui.draggable).text()).match(/\d+/g))); //using regular expression to get the string of the tile's value, and then converting it to an int
-            score += parseInt(($(ui.draggable).text()).match(/\d+/g)); //using regular expression to get the string of the tile's value, and then converting it to an int
+            currentSum += parseInt(($(ui.draggable).text()).match(/\d+/g)); //using regular expression to get the string of the tile's value, and then converting it to an int
+        }
+    })
+
+    $(".boardPieceTriple").droppable({ //for triple piece use
+
+        drop: function( event, ui ) {
+            //console.log($(this));
+            //console.log(parseInt(($(ui.draggable).text()).match(/\d+/g))); //using regular expression to get the string of the tile's value, and then converting it to an int
+            currentSum += parseInt(($(ui.draggable).text()).match(/\d+/g)); //using regular expression to get the string of the tile's value, and then converting it to an int
+            tripleCount++;
         }
     })
 
@@ -51,21 +64,34 @@ $(document).ready(function() {
         freq = ["A","A","A","A","A","A","A","A","A","B","B","C","C","D","D","D","D","E","E","E","E","E","E","E","E","E","E","E","E","F","F","G","G","G","H","H","I","I","I","I","I","I","I","I","I","J","K","L","L","L","L","M","M","N","N","N","N","N","N","O","O","O","O","O","O","O","O","P","P","Q","R","R","R","R","R","R","S","S","S","S","T","T","T","T","T","T","U","U","U","U","V","V","W","W","X","Y","Y","Z"," "," "];
         $("#hand").html("");
         dealHand(7);
-        resetScore();
+        updateScore(0);
+        currentSum = 0;
     });
 
+    // This is triggered when the user clicks end turn, and has (hopefully) placed some tiles
     $("#endButton").on("click", function (e) {
+        $("#warning").html('');
+        if (currentSum == 0) {
+            $("#warning").html('Please place at least one tile!');
+            return;
+        }
         var numTilesDealt = 7;
-        var newScore = score +
         $("#hand").html("");
         if(freq.length < 7) //If the tile bag has less than 7 tiles, the amount left will be dealt
             numTilesDealt = freq.length;
         dealHand(numTilesDealt);
-        resetScore();
+        currentSum *= (Math.pow(3,tripleCount));
+        score += currentSum;
+        tripleCount = 0;
+        currentSum = 0;
+        updateScore(score)
+
+
     });
 
-    function resetScore() {
-        $("#scoreArea").html("Score: "+ score);
+    function updateScore(n) {
+        score = n;
+        $("#scoreArea").html("Score: "+ n);
 
     }
 
